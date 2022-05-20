85b9d3de8c7daf808d6c6853a8163ef96c2255f7
LesiaBelinska <lm.kudak@gmail.com>
2022-05-20 20:10:40 +0300

    add persisStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,47 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+import {
+persistStore,
+persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+//import logger from "redux-logger";
+import storage from "redux-persist/lib/storage";
+import {  contactsReducer, filterReducer } from "./contacts-reducer.js";
 
-import { contactsReducer, filterReducer } from "./contacts-reducer.js";
+const contactsPersistConfig = {
+    key: 'contacts',
+    storage,
+    blacklist:['filter'], //blacklist - зберігати окрім такого поля, whitelist - зберігати тільки такі поля
+}
+
+export const persistedContactsReducer = persistReducer(
+    contactsPersistConfig,
+    contactsReducer
+);
 
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
+        contacts: persistedContactsReducer,
         filter: filterReducer,
     },
+    middleware (getDefaultMiddleware) {
+            return getDefaultMiddleware({
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST,
+                        PURGE, REGISTER],
+                },
+            });
+        }
     // прослойка, для виводу у консоль
-    middleware: getDefaultMiddleware =>
-        [...getDefaultMiddleware(), logger],
+    // middleware: getDefaultMiddleware =>
+    //     [...getDefaultMiddleware(), logger],
 });
 
+
+export const persistor = persistStore(store);
